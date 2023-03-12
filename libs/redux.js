fdae2f0a1b11787e3bf4f56d353eeff672ff6a1f
iosmonbekov <iosmonbekov666@gmail.com>
2023-03-12 18:16:05 +0600

    fixed combine reducers but with component render

--- libs/redux.js
+++ libs/redux.js
@@ -2,25 +2,25 @@ const Redux = (() => {
   function createStore(reducer, enhancer) {
     if (typeof enhancer === "function") return enhancer(createStore)(reducer);
 
-    let state = reducer(undefined, {})
+    let state = reducer(undefined, {});
     let subscriptions = [];
 
     return {
-        dispatch(action) {
-            state = reducer(state, action)
-            subscriptions.forEach((fn) => fn())
-        },
-        subscribe(fn) {
-            subscriptions.push(fn)
-            return () => {
-                const index = subscriptions.indexOf(fn)
-                subscriptions = subscriptions.splice(index, 1)
-            }
-        },
-        getState() {
-            return state
-        },
-    }
+      dispatch(action) {
+        state = reducer(state, action);
+        subscriptions.forEach((fn) => fn());
+      },
+      subscribe(fn) {
+        subscriptions.push(fn);
+        return () => {
+          const index = subscriptions.indexOf(fn);
+          subscriptions = subscriptions.splice(index, 1);
+        };
+      },
+      getState() {
+        return state;
+      },
+    };
   }
 
   function applyMiddleware(...fns) {
@@ -29,47 +29,46 @@ const Redux = (() => {
         const store = createStore(reducer);
 
         for (const fn of fns.reverse()) {
-            store.dispatch = fn(store)(store.dispatch)
+          store.dispatch = fn(store)(store.dispatch);
         }
 
-        return store
+        return store;
       };
     };
   }
 
   function combineReducers(reducers) {
     return (state = {}, action) => {
-        for (const key in reducers) {
-            state[key] = reducers[key](state[key], action)
-        }
-        return state
-    }
+      for (const key in reducers) {
+        state[key] = reducers[key](state[key], action);
+      }
+      return { ...state };
+    };
   }
 
   return { createStore, applyMiddleware, combineReducers };
 })();
 
-
 // Middlewares:
 const Middleware = (() => {
-    function logger(store) {
-        return (next) => {
-            return (action) => {
-                console.log('Action: ', action.type)
-                next(action)
-                console.log('State: ', store.getState())
-            }
-        }
-    }
+  function logger(store) {
+    return (next) => {
+      return (action) => {
+        console.log("Action: ", action.type);
+        next(action);
+        console.log("State: ", store.getState());
+      };
+    };
+  }
 
-    function saverLS(store) {
-      return (next) => {
-        return (action) => {
-          next(action)
-          localStorage.setItem('STORE', JSON.stringify(store.getState()))
-        }
-      }
-    } 
+  function saverLS(store) {
+    return (next) => {
+      return (action) => {
+        next(action);
+        localStorage.setItem("STORE", JSON.stringify(store.getState()));
+      };
+    };
+  }
 
-    return { logger, saverLS }
-})()
+  return { logger, saverLS };
+})();
